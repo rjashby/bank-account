e0ec654f624cb290fde7aa65ee573ac5bb4a915c
Ryan Ashby <rjapsu160@gmail.com>
2022-01-25 10:19:31 -0800

    add withdrawal and deposit functions
    Co-Authored-by: Chase Stokes <chasehstokes@gmail.com>
    Co-Authored By: Jack Jones <jacktjones4@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,33 @@ function BankAccount() {
   this.funds = 0;
 }
 
+BankAccount.prototype.addInitial = function(initialDeposit){
+  if (initialDeposit >= 0){
+    return this.funds += initialDeposit;
+  } else {
+    return "Please make an initial deposit."
+  }
+}
+
+BankAccount.prototype.addName = function(name){
+  return this.name = name;
+}
+
+BankAccount.prototype.addFunds = function(deposit) {
+  if (deposit >= 0) {
+    this.funds += deposit;
+  } else {
+    return "please enter a positive number"
+  }
+}
+
+BankAccount.prototype.withdrawFunds = function(withdrawal) {
+  if (withdrawal >= 0) {
+    this.funds -= withdrawal;
+  } else {
+    return "please enter a positive number"
+  }
+}
 
 // UI LOGIC
 
@@ -13,12 +40,26 @@ $(document).ready(function() {
   $("form#register").submit(function(event) {
     event.preventDefault();
     const myName = $("input#name").val();
-    const initialDeposit = parseInt($("input#initial").val())
+    const initialDeposit = parseInt($("input#initial").val());
+    myAccount.addName(myName);
+    myAccount.addInitial(initialDeposit);
+    $("input#name").val("");
+    $("input#initial").val("");
+    console.log(myName);
+    console.log(initialDeposit);
+    console.log(myAccount);
   })
 
   $("form#account").submit(function(event) {
     event.preventDefault();
-    const withdrawalAmount = parseInt($("input#deposit").val())
-    const depositAmount = parseInt($("input#withdrawal").val())
+    const withdrawalAmount = parseInt($("input#withdrawal").val())
+    const depositAmount = parseInt($("input#deposit").val())
+    myAccount.addFunds(depositAmount);
+    myAccount.withdrawFunds(withdrawalAmount);
+    $("input#withdrawal").val("");
+    $("input#deposit").val("");
+    console.log(withdrawalAmount);
+    console.log(depositAmount);
+    console.log(myAccount);
   })
 });
